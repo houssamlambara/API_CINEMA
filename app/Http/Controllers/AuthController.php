8e00077fb3924672aaa03794dfc88bdf9f50b342
houssamlambara <lambarahoussam@gmail.com>
2025-03-19 22:48:48 +0000

    udpdate

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:3|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        Auth::login($user);
+
+        $token = $user->creattoken('authToken')->plainTextToken;
+
+        return response()->json(['message' => 'Inscription réussie', 'user' => $user, 'token' => $token], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:3'
+        ]);
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+
+            $user = Auth::user();
+            $token = $user->createToken('authToken')->plainTextToken;
+            return response()->json(['token' => 'Email ou mot de passe incorrect'], 401);
+        }
+        return back()->withErrors(['email' => 'Email ou mot de passe incorrect']);
+    }
+
+    public function Logout(Request $request)
+    {
+        Auth::logout();
+        return response()->json(['message' => 'Déconnexion réussie'], 200);
+    }
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id) //GET
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id) // PUT
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id) //DELETE
+    {
+        //
+    }
+}
