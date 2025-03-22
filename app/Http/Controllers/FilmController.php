424712cb3ea81661e9677cc6909539b40626e1cd
houssamlambara <lambarahoussam@gmail.com>
2025-03-22 21:48:55 +0000

    ajout du filmController

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -2,37 +2,74 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Film;
+use App\Services\FilmService;
 use Illuminate\Http\Request;
 
 class FilmController extends Controller
 {
-    public function index()
+    protected $filmService;
+
+    public function __construct(FilmService $filmService)
     {
-        return response()->json(Film::all());
+        $this->filmService = $filmService;
     }
 
-    public function store(Request $request)
+    public function index()
     {
-        $film = Film::create($request->all());
-        return response()->json($film, 201);
+        return response()->json($this->filmService->getAllFilms());
     }
 
     public function show($id)
     {
-        return response()->json(Film::findOrFail($id));
+        return response()->json($this->filmService->getFilmById($id));
     }
+
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'titre' => 'required|string|max:255',
+            'description' => 'required|string',
+            'image' => 'required|string',
+            'duree' => 'required|string',
+            'age_minimum' => 'required|integer',
+            'bande_annonce' => 'string',
+            'acteur' => 'required|string',
+            'genre' => 'required|string',
+        ]);
+
+
+        return response()->json($this->filmService->createFilm($data), 201);
+    }
+
 
     public function update(Request $request, $id)
     {
-        $film = Film::findOrFail($id);
-        $film->update($request->all());
-        return response()->json($film);
+        $data = $request->validate([
+            'titre' => 'nullable|string|max:255',
+            'description' => 'nullable|string',
+            'image' => 'nullable|string',
+            'duree' => 'nullable|string',
+            'age_minimum' => 'nullable|integer',
+            'bande_annonce' => 'nullable|string',
+            'acteur' => 'nullable|string',
+            'genre' => 'nullable|string',
+        ]);
+
+        $updatedFilm = $this->filmService->updateFilm($id, $data);
+
+        return response()->json($updatedFilm);
     }
 
-    public function destroy($id)
-    {
-        Film::destroy($id);
-        return response()->json(['message' => 'Film supprimé']);
+
+    public function delete($id)
+{
+    $deleted = $this->filmService->deleteFilm($id);
+
+    if ($deleted) {
+        return response()->json(['message' => 'Film supprimé avec succès.'], 200);
+    } else {
+        return response()->json(['message' => 'Film introuvable ou déjà supprimé.'], 404);
     }
+}
+
 }
